14de606e541b3caf7bb9dd8e1fc1a0025ad3ee54
Nievl <rezalhbramantara@gmail.com>
2024-12-11 10:41:04 +0700

    modified:   nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  target: 'static',
+  router: {
+    base: '/pricelist-zalan-store', // Ganti dengan nama repository GitHub Anda
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/public/main.css'],
   modules: ['@nuxtjs/sitemap', '@nuxtjs/color-mode'],
   nitro: {
     preset: 'github_pages'
-  }
+  },
 })
